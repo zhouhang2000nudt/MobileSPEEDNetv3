bb0037f5db3da2bf8729e4d82782fbcd7eb7e259
Machain <863101876@qq.com>
2024-05-31 16:17:07 +0800

    该版本在取出图像后重新resize，以方便展示 在后续版本中，可能不会再把图像resize回原图大小，而是直接展示

--- test/test_augment.py
+++ test/test_augment.py
@@ -1,10 +1,14 @@
 import sys
 sys.path.insert(0, sys.path[0]+"/../")
 
+import numpy as np
+
 from MobileSPEEDNetv3.utils.utils import Camera
 from MobileSPEEDNetv3.utils.config import get_config
 from MobileSPEEDNetv3.utils.dataset import Speed, prepare_Speed
 from MobileSPEEDNetv3.utils.vis import visualize
+from torchvision.transforms.v2 import ToPILImage
+
 
 category_ids = [1]
 category_id_to_name = {1: 'satellite'}
@@ -19,6 +23,9 @@ for i in range(len(speed)):
     break
 print(y["filename"])
 print(image.shape)
+image_pil = ToPILImage()(image)
+image_pil = image_pil.resize((1920, 1200))
+image = np.array(image_pil)
 pos = y["pos"]
 ori = y["ori"]
 bbox = y["bbox"]
