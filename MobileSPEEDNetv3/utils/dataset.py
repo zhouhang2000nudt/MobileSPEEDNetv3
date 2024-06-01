03655d91cdb4cb7c685e6d163c9c16fe0540e08f
Machain <863101876@qq.com>
2024-06-01 18:22:25 +0800

    异步加载数据

--- MobileSPEEDNetv3/utils/dataset.py
+++ MobileSPEEDNetv3/utils/dataset.py
@@ -6,7 +6,7 @@ from tqdm import tqdm
 from numpy import ndarray
 from .utils import rotate_image, rotate_cam, Camera, wrap_boxes, bbox_in_image, OriEncoderDecoder
 from typing import List
-from pl_bolts.datamodules.async_dataloader import AsynchronousLoader as AsyncDataLoader
+from queue import Queue
 
 import albumentations as A
 import cv2 as cv
@@ -34,6 +34,77 @@ def CropAndPad(img: np.array, bbox: List[float]):
     img = cv.copyMakeBorder(img, 0, 0, 0, width-crop_x_max-1, cv.BORDER_REPLICATE)
     return img
 
+
+class CudaDataLoader:
+    """ 异步预先将数据从 CPU 加载到 GPU 中 """
+
+    def __init__(self, loader, device, queue_size=2):
+        self.device = device
+        self.queue_size = queue_size
+        self.loader = loader
+
+        self.load_stream = torch.cuda.Stream(device=device)
+        self.queue = Queue(maxsize=self.queue_size)
+
+        self.idx = 0
+        self.worker = Thread(target=self.load_loop)
+        self.worker.setDaemon(True)
+        self.worker.start()
+
+    def load_loop(self):
+        """ 不断的将 cuda 数据加载到队列里 """
+        # The loop that will load into the queue in the background
+        while True:
+            for i, sample in enumerate(self.loader):
+                self.queue.put(self.load_instance(sample))
+
+    def load_instance(self, sample):
+        """ 将 batch 数据从 CPU 加载到 GPU 中 """
+        if torch.is_tensor(sample):
+            with torch.cuda.stream(self.load_stream):
+                return sample.to(self.device, non_blocking=True)
+        elif sample is None or type(sample) in (list, str):
+            return sample
+        elif isinstance(sample, dict):
+            return {k: self.load_instance(v) for k, v in sample.items()}
+        else:
+            return [self.load_instance(s) for s in sample]
+
+    def __iter__(self):
+        self.idx = 0
+        return self
+
+    def __next__(self):
+        # 加载线程意外退出了
+        if not self.worker.is_alive() and self.queue.empty():
+            self.idx = 0
+            self.queue.join()
+            self.worker.join()
+            raise StopIteration
+        # 一个 epoch 加载完了
+        elif self.idx >= len(self.loader):
+            self.idx = 0
+            raise StopIteration
+        # 下一个 batch
+        else:
+            out = self.queue.get()
+            self.queue.task_done()
+            self.idx += 1
+        return out
+
+    def next(self):
+        return self.__next__()
+
+    def __len__(self):
+        return len(self.loader)
+
+    @property
+    def sampler(self):
+        return self.loader.sampler
+
+    @property
+    def dataset(self):
+        return self.loader.dataset
 
 class MultiEpochsDataLoader(torch.utils.data.DataLoader):
     def __init__(self, *args, **kwargs):
@@ -405,7 +476,7 @@ class SpeedDataModule(L.LightningDataModule):
             self.speed_data_val: Speed = Speed("val")
     
     def train_dataloader(self) -> MultiEpochsDataLoader:
-        return MultiEpochsDataLoader(
+        loader = MultiEpochsDataLoader(
             self.speed_data_train,
             batch_size=self.config["batch_size"],
             shuffle=True,
@@ -413,9 +484,10 @@ class SpeedDataModule(L.LightningDataModule):
             persistent_workers=True,
             pin_memory=True
         )
+        return CudaDataLoader(loader, 'cuda', queue_size=4)
     
     def val_dataloader(self) -> MultiEpochsDataLoader:
-        return MultiEpochsDataLoader(
+        loader = MultiEpochsDataLoader(
             self.speed_data_val,
             batch_size=self.config["batch_size"] * 2,
             shuffle=False,
@@ -423,6 +495,7 @@ class SpeedDataModule(L.LightningDataModule):
             persistent_workers=True,
             pin_memory=True
         )
+        return CudaDataLoader(loader, 'cuda', queue_size=4)
 
 
 
