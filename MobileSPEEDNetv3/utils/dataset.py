b7fba16673cb8a13a304d9f4aeed2565834e38f5
Machain <863101876@qq.com>
2024-06-08 13:27:43 +0800

    修改dataloader

--- MobileSPEEDNetv3/utils/dataset.py
+++ MobileSPEEDNetv3/utils/dataset.py
@@ -15,6 +15,7 @@ import numpy as np
 
 import json
 import torch
+from prefetch_generator import BackgroundGenerator
 
 
 
@@ -62,77 +63,6 @@ def DropBlockSafe(img: np.array, bbox: List[float], drop_num_lim: int):
             pass
     return img
 
-class CudaDataLoader:
-    """ 异步预先将数据从 CPU 加载到 GPU 中 """
-
-    def __init__(self, loader, device, queue_size=2):
-        self.device = device
-        self.queue_size = queue_size
-        self.loader = loader
-
-        self.load_stream = torch.cuda.Stream(device=device)
-        self.queue = Queue(maxsize=self.queue_size)
-
-        self.idx = 0
-        self.worker = Thread(target=self.load_loop)
-        self.worker.setDaemon(True)
-        self.worker.start()
-
-    def load_loop(self):
-        """ 不断的将 cuda 数据加载到队列里 """
-        # The loop that will load into the queue in the background
-        while True:
-            for i, sample in enumerate(self.loader):
-                self.queue.put(self.load_instance(sample))
-
-    def load_instance(self, sample):
-        """ 将 batch 数据从 CPU 加载到 GPU 中 """
-        if torch.is_tensor(sample):
-            with torch.cuda.stream(self.load_stream):
-                return sample.to(self.device, non_blocking=True)
-        elif sample is None or type(sample) in (list, str):
-            return sample
-        elif isinstance(sample, dict):
-            return {k: self.load_instance(v) for k, v in sample.items()}
-        else:
-            return [self.load_instance(s) for s in sample]
-
-    def __iter__(self):
-        self.idx = 0
-        return self
-
-    def __next__(self):
-        # 加载线程意外退出了
-        if not self.worker.is_alive() and self.queue.empty():
-            self.idx = 0
-            self.queue.join()
-            self.worker.join()
-            raise StopIteration
-        # 一个 epoch 加载完了
-        elif self.idx >= len(self.loader):
-            self.idx = 0
-            raise StopIteration
-        # 下一个 batch
-        else:
-            out = self.queue.get()
-            self.queue.task_done()
-            self.idx += 1
-        return out
-
-    def next(self):
-        return self.__next__()
-
-    def __len__(self):
-        return len(self.loader)
-
-    @property
-    def sampler(self):
-        return self.loader.sampler
-
-    @property
-    def dataset(self):
-        return self.loader.dataset
-
 class MultiEpochsDataLoader(torch.utils.data.DataLoader):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -160,6 +90,12 @@ class _RepeatSampler(object):
     def __iter__(self):
         while True:
             yield from iter(self.sampler)
+
+
+class DataLoaderX(torch.utils.data.DataLoader):
+
+    def __iter__(self):
+        return BackgroundGenerator(super().__iter__())
 
 
 def prepare_Speed(config: dict):
@@ -541,7 +477,7 @@ class SpeedDataModule(L.LightningDataModule):
         #     pin_memory_device='cuda',
         #     prefetch_factor=5
         # )
-        loader = torch.utils.data.DataLoader(
+        loader = DataLoaderX(
             self.speed_data_train,
             batch_size=self.config["batch_size"],
             shuffle=True,
@@ -551,11 +487,10 @@ class SpeedDataModule(L.LightningDataModule):
             pin_memory_device='cuda',
             prefetch_factor=5
         )
-        # return CudaDataLoader(loader, 'cuda', queue_size=10)
         return loader
     
     def val_dataloader(self) -> MultiEpochsDataLoader:
-        # loader = MultiEpochsDataLoader(
+        # loader = torch.utils.data.DataLoader(
         #     self.speed_data_val,
         #     batch_size=self.config["batch_size"] * 2,
         #     shuffle=False,
@@ -565,7 +500,7 @@ class SpeedDataModule(L.LightningDataModule):
         #     pin_memory_device='cuda',
         #     prefetch_factor=5
         # )
-        loader = torch.utils.data.DataLoader(
+        loader = DataLoaderX(
             self.speed_data_val,
             batch_size=self.config["batch_size"] * 2,
             shuffle=False,
@@ -575,7 +510,6 @@ class SpeedDataModule(L.LightningDataModule):
             pin_memory_device='cuda',
             prefetch_factor=5
         )
-        # return CudaDataLoader(loader, 'cuda', queue_size=10)
         return loader
 
 
